a5491645e8526d340f750021f195f9cfc87fcbef
Noel Miller <nomiller@redhat.com>
2024-02-02 10:01:34 -0600

    feat: added my aliases

---
+++ files/etc/profile.d/isengard-aliases.sh
@@ -0,0 +1,25 @@
+alias butane='podman run --rm --interactive         \
+              --security-opt label=disable          \
+              --volume "${PWD}":/pwd --workdir /pwd \
+              quay.io/coreos/butane:release'
+
+# ===============================
+# Ansible aliases (ansible-navigator)
+# ===============================
+
+alias anr="ansible-navigator run"
+alias ansc="ansible-navigator run --syntax-check"
+alias anc="ansible-navigator run --check"
+alias acd="ansible-navigator config dump"
+alias av="ansible --version"
+alias aig="ansible-navigator inventory --graph"
+
+# ================
+# General aliases
+# ================
+
+# stop using scp
+alias scp="echo -e \nStop using scp\n\nSee https://www.redhat.com/en/blog/openssh-scp-deprecation-rhel-9-what-you-need-know"
+
+# decomment a config file
+alias decomment='grep -v -E "^[[:space:]]*((#|;|//).*)?$" '
