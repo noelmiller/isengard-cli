7c592ee1400a1491d2a743f21d480411122e7e67
Noel Miller <noelmiller@protonmail.com>
2024-02-19 14:19:12 -0600

    fix: Updated message in terminal

--- files/etc/profile.d/01-isengard-xdg.sh
+++ files/etc/profile.d/01-isengard-xdg.sh
@@ -4,6 +4,6 @@ export XDG_STATE_HOME=/XDG_DIRS/local/state
 export XDG_CACHE_HOME=/XDG_DIRS/cache
 
 if [ ! -w "/XDG_DIRS/config/gh" ] || [ ! -w "/XDG_DIRS/config/github-copilot"] || [ ! -w "/XDG_DIRS/local/share/zoxide"]; then
-	echo "XDG_DIRS is not writable, please enter your password to enable read/write/execute permissions"
+	echo "Fixing XDG_DIRS permissions..."
 	sudo chmod -R o+rwx /XDG_DIRS
 fi
