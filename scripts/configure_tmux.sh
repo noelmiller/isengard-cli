2e0da1681869059ea9623857fa8fce7736db3a96
Noel Miller <noelmiller@protonmail.com>
2024-03-29 15:15:48 -0500

    fix: move tmux config move above other commands

--- scripts/configure_tmux.sh
+++ scripts/configure_tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /etc/profile.d/01-isengard-xdg.sh
-git clone https://github.com/tmux-plugins/tpm /XDG_DIRS/config/tmux/plugins/tpm
 mv /tmux /XDG_DIRS/config/tmux
+git clone https://github.com/tmux-plugins/tpm /XDG_DIRS/config/tmux/plugins/tpm
 /XDG_DIRS/config/tmux/plugins/tpm/bin/install_plugins
