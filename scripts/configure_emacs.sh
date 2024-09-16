5542e2c5c1282aaea524ad503730cdc4a1a48cd7
Noel Miller <noelmiller@protonmail.com>
2024-09-16 00:33:46 -0500

    fix: add yes

--- scripts/configure_emacs.sh
+++ scripts/configure_emacs.sh
@@ -6,5 +6,5 @@ source /etc/profile.d/01-isengard-xdg.sh
 echo -e "${GREEN}### Configuring emacs.. ###"
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs /XDG_DIRS/config/emacs
-/XDG_DIRS/config/emacs/bin/doom install
+/XDG_DIRS/config/emacs/bin/doom --yes install
 #mv -v /isengard-nvim /XDG_DIRS/config/nvim
