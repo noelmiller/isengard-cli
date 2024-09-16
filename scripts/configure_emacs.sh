ed71c81a9176652e470c4896195b1652b00a2e22
Noel Miller <noelmiller@protonmail.com>
2024-09-16 00:39:30 -0500

    fix: try just sync

--- scripts/configure_emacs.sh
+++ scripts/configure_emacs.sh
@@ -6,5 +6,5 @@ source /etc/profile.d/01-isengard-xdg.sh
 echo -e "${GREEN}### Configuring emacs.. ###"
 
 git clone --depth 1 https://github.com/doomemacs/doomemacs /XDG_DIRS/config/emacs
-/XDG_DIRS/config/emacs/bin/doom -y sync
+/XDG_DIRS/config/emacs/bin/doom sync
 #mv -v /isengard-nvim /XDG_DIRS/config/nvim
