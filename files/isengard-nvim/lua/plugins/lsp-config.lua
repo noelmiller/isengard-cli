3d9668b07a1bbfd25be92bd95049e8401c961ba7
Noel Miller <noelmiller@protonmail.com>
2024-09-16 03:04:42 -0500

    feat: add LSP support and theme back

---
+++ files/isengard-nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim"
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ansiblels",
+          "bashls",
+          "cssls",
+          "dockerls",
+          "gopls",
+          "html",
+          "jsonls",
+          "lua_ls",
+          "pylsp",
+          "tailwindcss",
+          "yamlls",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.ansiblels.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.pylsp.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.yamlls.setup({})
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+      })
+    end
+  }
+}
